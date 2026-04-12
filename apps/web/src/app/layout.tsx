3139730a6fd7d163983299bf257dbed58809e3d0
Esalu <esalu@profedex.top>
2026-04-11 19:18:25 -0600

    fix: Change font import and implementation, and change background color to match hero ui theme

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,21 +1,12 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Manrope } from 'next/font/google';
 import './globals.css';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import { Manrope } from 'next/font/google';
 
 const manrope = Manrope({
   variable: '--font-manrope',
   subsets: ['latin'],
-})
+  weight: ['400', '600', '800'],
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -29,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={`${manrope.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${manrope.className} antialiased bg-background`}>{children}</body>
     </html>
   );
 }
