0a9e33f4b851a63e770386cbbf25dac96316fd1a
Esalu <esalu@profedex.top>
2026-05-14 20:38:04 -0600

    feat: Enhance main layout with TopBar component and improve sidebar structure

--- apps/web/src/app/app/layout.tsx
+++ apps/web/src/app/app/layout.tsx
@@ -1,49 +1,49 @@
-import { SidebarButton } from "@/components/dashboard/sidebarButton";
-import { ChartColumnStacked, FileDollar, ShoppingBasket, ShoppingCart, Thunderbolt, Trolley } from "@gravity-ui/icons";
+import { SidebarButton } from '@/components/dashboard/sidebarButton';
+import { TopBar } from '@/components/dashboard/TopBar';
+import {
+  ChartColumnStacked,
+  FileDollar,
+  ShoppingBasket,
+  ShoppingCart,
+  Thunderbolt,
+  Trolley,
+} from '@gravity-ui/icons';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className="flex h-dvh w-dvw">
-            <aside className="flex h-full w-80 flex-col px-6 py-8 items-center bg-focus">
-                <h3 className="text-center text-3xl text-white font-bold">The Famous Pizza and Beer</h3>
-                <p className="text-center text-white mt-2">Enterprise Resource Planner</p>
-                <section className="mt-10 w-full">
-                    <ul className="flex flex-col gap-2">
-                        <li>
-                            <SidebarButton icon={<ChartColumnStacked />} selected>
-                            Inicio
-                            </SidebarButton>
-                        </li>
-                        <li>
-                            <SidebarButton icon={<FileDollar />}>   
-                            Precios
-                            </SidebarButton>
-                        </li>
-                        <li>
-                            <SidebarButton icon={<ShoppingCart />}>
-                            Insumos
-                            </SidebarButton>
-                        </li>
-                        <li>
-                            <SidebarButton icon={<Thunderbolt />}>   
-                            Servicios
-                            </SidebarButton>
-                        </li>
-                        <li>
-                            <SidebarButton icon={<ShoppingBasket />}>
-                            Productos
-                            </SidebarButton>
-                        </li>
-                        <li>
-                            <SidebarButton icon={<Trolley />}>   
-                            Proveedores
-                            </SidebarButton></li>
-                    </ul>
-                </section>
-            </aside>
-            <main className="flex size-full">
-                {children}
-            </main>
-        </div>
-    )
+  return (
+    <div className="flex h-dvh max-w-dvw relative">
+      <aside className="flex h-full w-80 flex-col px-6 py-8 items-center bg-focus">
+        <h3 className="text-center text-3xl text-white font-bold">The Famous Pizza and Beer</h3>
+        <p className="text-center text-white mt-2">Enterprise Resource Planner</p>
+        <section className="mt-10 w-full">
+          <ul className="flex flex-col gap-2">
+            <li>
+              <SidebarButton icon={<ChartColumnStacked />} selected>
+                Inicio
+              </SidebarButton>
+            </li>
+            <li>
+              <SidebarButton icon={<FileDollar />}>Precios</SidebarButton>
+            </li>
+            <li>
+              <SidebarButton icon={<ShoppingCart />}>Insumos</SidebarButton>
+            </li>
+            <li>
+              <SidebarButton icon={<Thunderbolt />}>Servicios</SidebarButton>
+            </li>
+            <li>
+              <SidebarButton icon={<ShoppingBasket />}>Productos</SidebarButton>
+            </li>
+            <li>
+              <SidebarButton icon={<Trolley />}>Proveedores</SidebarButton>
+            </li>
+          </ul>
+        </section>
+      </aside>
+      <main className="flex flex-col outline relative w-full">
+        <TopBar />
+        <div className="px-6 py-4">{children}</div>
+      </main>
+    </div>
+  );
 }
