f94c373ab0848e20caafd5c2513ad6186da75bb5
Esalu <esalu@profedex.top>
2026-05-28 20:15:56 -0600

    fix: delete extra div tag

--- apps/web/src/app/app/layout.tsx
+++ apps/web/src/app/app/layout.tsx
@@ -18,58 +18,53 @@ export default async function MainLayout({ children }: { children: React.ReactNo
 
   return (
     <AuthProvider initialUser={session}>
-     <div className="flex min-h-dvh max-w-dvw relative">
       <div className="flex min-h-dvh max-w-dvw relative">
-       <aside className="sticky top-0 flex h-dvh w-80 flex-col px-6 py-8 items-center bg-surface">
-        <div>
-          <img
-            src={logo.src}
-            alt="Logo de The Famous Pizza and Beer"
-            className="h-24 w-auto"
-          />
-        </div>
-        <p className="text-start text-gray-600 mt-2 text-xs">Enterprise Resource Planner</p>
-        <section className="mt-10 w-full">
-          <ul className="flex flex-col gap-2">
-            <li>
-              <SidebarButton icon={<ChartColumnStacked />} route="/app">
-                Inicio
-              </SidebarButton>
-            </li>
-            <li>
-              <SidebarButton icon={<FileDollar />} route="/precios">
-                Precios
-              </SidebarButton>
-            </li>
-            <li>
-              <SidebarButton icon={<ShoppingCart />} route="/app/insumos">
-                Insumos
-              </SidebarButton>
-            </li>
-            <li>
-              <SidebarButton icon={<Thunderbolt />} route="/app/servicios">
-                Servicios
-              </SidebarButton>
-            </li>
-            <li>
-              <SidebarButton icon={<ShoppingBasket />} route="/app/productos">
-                Productos
-              </SidebarButton>
-            </li>
-            <li>
-              <SidebarButton icon={<Trolley />} route="/app/proveedores">
-                Proveedores
-              </SidebarButton>
-            </li>
-          </ul>
-        </section>
-      </aside>
-      <main className="flex flex-col relative w-full">
-        <TopBar />
-        <Separator />
-        <div className="px-6 py-4">{children}</div>
-      </main>
-    </div>
+        <aside className="sticky top-0 flex h-dvh w-80 flex-col px-6 py-8 items-center bg-surface">
+          <div>
+            <img src={logo.src} alt="Logo de The Famous Pizza and Beer" className="h-24 w-auto" />
+          </div>
+          <p className="text-start text-gray-600 mt-2 text-xs">Enterprise Resource Planner</p>
+          <section className="mt-10 w-full">
+            <ul className="flex flex-col gap-2">
+              <li>
+                <SidebarButton icon={<ChartColumnStacked />} route="/app">
+                  Inicio
+                </SidebarButton>
+              </li>
+              <li>
+                <SidebarButton icon={<FileDollar />} route="/precios">
+                  Precios
+                </SidebarButton>
+              </li>
+              <li>
+                <SidebarButton icon={<ShoppingCart />} route="/app/insumos">
+                  Insumos
+                </SidebarButton>
+              </li>
+              <li>
+                <SidebarButton icon={<Thunderbolt />} route="/app/servicios">
+                  Servicios
+                </SidebarButton>
+              </li>
+              <li>
+                <SidebarButton icon={<ShoppingBasket />} route="/app/productos">
+                  Productos
+                </SidebarButton>
+              </li>
+              <li>
+                <SidebarButton icon={<Trolley />} route="/app/proveedores">
+                  Proveedores
+                </SidebarButton>
+              </li>
+            </ul>
+          </section>
+        </aside>
+        <main className="flex flex-col relative w-full">
+          <TopBar />
+          <Separator />
+          <div className="px-6 py-4">{children}</div>
+        </main>
+      </div>
     </AuthProvider>
   );
 }
