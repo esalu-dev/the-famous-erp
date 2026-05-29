a1b29ce31d5597fec8d1d91ac24d21cb7f60e6be
Esalu <esalu@profedex.top>
2026-05-28 20:01:37 -0600

    feat(proveedores): update component imports and improve formatting in ProveedoresGrid, ProveedorCard, and AddProveedorButton

--- apps/web/src/components/proveedores/proveedoresGrid.tsx
+++ apps/web/src/components/proveedores/proveedoresGrid.tsx
@@ -9,9 +9,30 @@ export function ProveedoresGrid() {
         <ComparadorCard />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-1 xl:grid-cols-2 gap-4 mt-6">
-        <ProveedorCard nombre="Lacteos Express" razonSocial="Lacteos Express S.A." tipoProveedor="Proveedor A" telefono="123-456-7890" email="proveedor1@example.com" estado="Activo" />
-        <ProveedorCard nombre="Lacteos Express" razonSocial="Lacteos Express S.A." tipoProveedor="Proveedor A" telefono="123-456-7890" email="proveedor1@example.com" estado="Inactivo" />
-        <ProveedorCard nombre="Lacteos Express" razonSocial="Lacteos Express S.A." tipoProveedor="Proveedor A" telefono="123-456-7890" email="proveedor1@example.com" estado="Activo" />
+        <ProveedorCard
+          nombre="Lacteos Express"
+          razonSocial="Lacteos Express S.A."
+          tipoProveedor="Proveedor A"
+          telefono="123-456-7890"
+          email="proveedor1@example.com"
+          estado="Activo"
+        />
+        <ProveedorCard
+          nombre="Lacteos Express"
+          razonSocial="Lacteos Express S.A."
+          tipoProveedor="Proveedor A"
+          telefono="123-456-7890"
+          email="proveedor1@example.com"
+          estado="Inactivo"
+        />
+        <ProveedorCard
+          nombre="Lacteos Express"
+          razonSocial="Lacteos Express S.A."
+          tipoProveedor="Proveedor A"
+          telefono="123-456-7890"
+          email="proveedor1@example.com"
+          estado="Activo"
+        />
       </div>
       <AddProveedorButton />
     </>
